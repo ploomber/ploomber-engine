465cc66e1470527ed3f7d205b1c565d586777fdf
idomi <michael.ido@gmail.com>
2022-12-14 13:22:02 -0500

    review comments

--- src/ploomber_engine/tracking/telemetry.py
+++ src/ploomber_engine/tracking/telemetry.py
@@ -1,8 +1,12 @@
 from ploomber_core.telemetry.telemetry import Telemetry
-from ploomber_engine import __version__
+
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
 
 telemetry = Telemetry(
     api_key="phc_P9SpSeypyPwxrMdFn2edOOEooQioF2axppyEeDwtMSP",
     package_name="ploomber-engine",
-    version=__version__,
+    version=version("ploomber_engine"),
 )
