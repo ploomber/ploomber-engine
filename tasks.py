44970422934e27227a5804267de0ead163041abc
Eduardo Blancas <github@blancas.io>
2022-12-17 13:38:31 -0600

    adds command to build docs to tasks.py

--- tasks.py
+++ tasks.py
@@ -1,34 +1,42 @@
 from invoke import task
 
 
-@task
+@task(aliases=["s"])
 def setup(c, version=None):
     """
     Setup dev environment, requires conda
     """
-    version = version or '3.9'
-    suffix = '' if version == '3.9' else version.replace('.', '')
-    env_name = f'ploomber-engine{suffix}'
+    version = version or "3.9"
+    suffix = "" if version == "3.9" else version.replace(".", "")
+    env_name = f"ploomber-engine{suffix}"
 
-    c.run(f'conda create --name {env_name} python={version} --yes')
-    c.run('eval "$(conda shell.bash hook)" '
-          f'&& conda activate {env_name} '
-          '&& pip install --editable .[dev]')
+    c.run(f"conda create --name {env_name} python={version} --yes")
+    c.run(
+        'eval "$(conda shell.bash hook)" '
+        f"&& conda activate {env_name} "
+        "&& pip install --editable .[dev]"
+    )
 
-    print(f'Done! Activate your environment with:\nconda activate {env_name}')
+    print(f"Done! Activate your environment with:\nconda activate {env_name}")
 
 
-@task(aliases=['v'])
+@task(aliases=["d"])
+def doc(c):
+    """Build documentation"""
+    c.run("jupyter-book build doc/")
+
+
+@task(aliases=["v"])
 def version(c):
-    """Release a new version
-    """
+    """Release a new version"""
     from pkgmt import versioneer
-    versioneer.version(project_root='.', tag=True)
+
+    versioneer.version(project_root=".", tag=True)
 
 
-@task(aliases=['r'])
+@task(aliases=["r"])
 def release(c, tag, production=True):
-    """Upload to PyPI
-    """
+    """Upload to PyPI"""
     from pkgmt import versioneer
+
     versioneer.upload(tag, production=production)
