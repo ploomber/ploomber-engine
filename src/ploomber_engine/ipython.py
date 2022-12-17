8080e7e78f72f2795f797aa14b0f9115016f2904
Eduardo Blancas <github@blancas.io>
2022-12-17 16:47:17 -0600

    adds missing docstrings and doctests

--- src/ploomber_engine/ipython.py
+++ src/ploomber_engine/ipython.py
@@ -12,9 +12,7 @@ from IPython.core.displayhook import DisplayHook
 
 
 def _make_stream_output(out, name):
-    return nbformat.v4.new_output(output_type='stream',
-                                  text=str(out),
-                                  name=name)
+    return nbformat.v4.new_output(output_type="stream", text=str(out), name=name)
 
 
 def _process_stdout(out, result):
@@ -27,22 +25,26 @@ def _process_stdout(out, result):
 
         if out:
             cells.append(
-                nbformat.v4.new_output(output_type='stream',
-                                       text='\n'.join(out),
-                                       name='stdout'))
+                nbformat.v4.new_output(
+                    output_type="stream", text="\n".join(out), name="stdout"
+                )
+            )
 
         cells.append(
-            nbformat.v4.new_output("error",
-                                   ename=type(result.error_in_exec).__name__,
-                                   evalue=str(result.error_in_exec),
-                                   traceback=exception.splitlines()))
+            nbformat.v4.new_output(
+                "error",
+                ename=type(result.error_in_exec).__name__,
+                evalue=str(result.error_in_exec),
+                traceback=exception.splitlines(),
+            )
+        )
         return cells
 
     else:
         return [
-            nbformat.v4.new_output(output_type='stream',
-                                   text=''.join(out),
-                                   name='stdout')
+            nbformat.v4.new_output(
+                output_type="stream", text="".join(out), name="stdout"
+            )
         ]
 
 
@@ -54,9 +56,9 @@ class CustomDisplayHook(DisplayHook):
     def __call__(self, result=None):
         if result is not None:
             data, metadata = self.shell.display_formatter.format(result)
-            out = nbformat.v4.new_output(output_type='execute_result',
-                                         data=data,
-                                         metadata=metadata)
+            out = nbformat.v4.new_output(
+                output_type="execute_result", data=data, metadata=metadata
+            )
             self.shell._current_output.append(out)
 
 
@@ -68,7 +70,7 @@ class CustomDisplayPublisher(DisplayPublisher):
     """
 
     def publish(self, data, metadata=None, **kwargs):
-        out = nbformat.v4.new_output(output_type='display_data', data=data)
+        out = nbformat.v4.new_output(output_type="display_data", data=data)
         self.shell._current_output.append(out)
 
 
@@ -84,15 +86,17 @@ class PloomberShell(InteractiveShell):
     """
 
     def __init__(self):
-        super().__init__(display_pub_class=CustomDisplayPublisher,
-                         displayhook_class=CustomDisplayHook)
+        super().__init__(
+            display_pub_class=CustomDisplayPublisher,
+            displayhook_class=CustomDisplayHook,
+        )
         # no assigning this produces some weird behavior when calling
         # .clear_instance()
         InteractiveShell._instance = self
         PloomberShell._instance = self
 
         try:
-            self.enable_matplotlib('inline')
+            self.enable_matplotlib("inline")
         except ModuleNotFoundError:
             pass
 
@@ -116,7 +120,8 @@ class PloomberShell(InteractiveShell):
         # if we don't put this, we'll lose some display_data messages. found
         # about this trick via fastai/execnb
         from matplotlib_inline.backend_inline import configure_inline_support
-        configure_inline_support.current_backend = 'unset'
+
+        configure_inline_support.current_backend = "unset"
         return super().enable_matplotlib(gui)
 
     def __enter__(self):
@@ -127,9 +132,27 @@ class PloomberShell(InteractiveShell):
 
 
 class PloomberClient:
-    """
-    Partially implements nbclient.client's interface and uses PloomberShell
-    to execute cells.
+    """PloomberClient executes Jupyter notebooks
+
+    Parameters
+    ----------
+    nb
+        Notebook object
+
+    display_output : bool, default=False
+        If True, it prints whatever output the notebook prints.
+
+    Examples
+    --------
+    >>> from ploomber_engine.ipython import PloomberClient
+    >>> import nbformat
+    >>> nb = nbformat.v4.new_notebook()
+    >>> cell = nbformat.v4.new_code_cell("1+1")
+    >>> nb.cells = [cell]
+    >>> client = PloomberClient(nb)
+    >>> out = client.execute()
+    >>> out.cells[0]['outputs'][0]['data']
+    {'text/plain': '2'}
     """
 
     def __init__(self, nb, display_stdout=False):
@@ -139,17 +162,32 @@ class PloomberClient:
 
     @classmethod
     def from_path(cls, path):
+        """Initialize client from a path to a notebook
+
+
+        Parameters
+        ----------
+        path : str
+            Path to the ``.ipynb`` file
+
+
+        Examples
+        --------
+        >>> from ploomber_engine.ipython import PloomberClient
+        >>> client = PloomberClient.from_path("examples/nb.ipynb")
+        >>> out = client.execute()
+        """
         nb = nbformat.read(path, as_version=nbformat.NO_CONVERT)
         return cls(nb)
 
     def execute_cell(self, cell, cell_index, execution_count, store_history):
         if self._shell is None:
-            raise RuntimeError('A shell has not been initialized')
+            raise RuntimeError("A shell has not been initialized")
 
         # results are published in different places. Here we grab all of them
         # and return them
         with patch_sys_std_out_err() as (stdout_stream, stderr_stream):
-            result = self._shell.run_cell(cell['source'])
+            result = self._shell.run_cell(cell["source"])
             stdout = stdout_stream.get_separated_values()
             stderr = stderr_stream.getvalue()
 
@@ -158,12 +196,12 @@ class PloomberClient:
         if stdout:
 
             if self._display_stdout:
-                print(''.join([line.strip() for line in stdout]))
+                print("".join([line.strip() for line in stdout]))
 
             output.extend(_process_stdout(stdout, result=result))
 
         if stderr:
-            output.append(_make_stream_output(stderr, name='stderr'))
+            output.append(_make_stream_output(stderr, name="stderr"))
 
         # order is important, this should be the last one to match
         # what jupyter does
@@ -171,8 +209,8 @@ class PloomberClient:
 
         # NOTE: is there any situation where we receive more than one?
         for current in out:
-            if current['output_type'] == 'execute_result':
-                current['execution_count'] = execution_count
+            if current["output_type"] == "execute_result":
+                current["execution_count"] = execution_count
 
         output = output + out
 
@@ -187,6 +225,11 @@ class PloomberClient:
 
     def execute(self):
         """Execute the notebook
+
+        Returns
+        -------
+        nb
+            Notebook object
         """
         with self:
             self._execute()
@@ -194,39 +237,70 @@ class PloomberClient:
         return self._nb
 
     def get_namespace(self):
-        """Run the notebook and return all the variables
+        """Run the notebook and return all the output variables
+
+        Examples
+        --------
+        >>> from ploomber_engine.ipython import PloomberClient
+        >>> import nbformat
+        >>> nb = nbformat.v4.new_notebook()
+        >>> first = nbformat.v4.new_code_cell("x = 1")
+        >>> second = nbformat.v4.new_code_cell("y = 41")
+        >>> third = nbformat.v4.new_code_cell("z = x + y")
+        >>> nb.cells = [first, second, third]
+        >>> client = PloomberClient(nb)
+        >>> ns = client.get_namespace()
+        >>> ns["x"]
+        1
+        >>> ns["y"]
+        41
+        >>> ns["z"]
+        42
         """
         with self:
             existing = set(self._shell.user_ns)
             self._execute()
             namespace = {
-                k: v
-                for k, v in self._shell.user_ns.items() if k not in existing
+                k: v for k, v in self._shell.user_ns.items() if k not in existing
             }
 
         return namespace
 
     def get_definitions(self):
-        """Returns class and function definitions without running the notebook
         """
-        source = '\n'.join(
-            [c.source for c in self._nb.cells if c.cell_type == 'code'])
+        Returns class and function definitions without running the notebook
+
+        Examples
+        --------
+        >>> from ploomber_engine.ipython import PloomberClient
+        >>> import nbformat
+        >>> nb = nbformat.v4.new_notebook()
+        >>> first = nbformat.v4.new_code_cell("def add(x, y): return x + y")
+        >>> second = nbformat.v4.new_code_cell("def multiply(x, y): return x * y")
+        >>> nb.cells = [first, second]
+        >>> client = PloomberClient(nb)
+        >>> defs = client.get_definitions()
+        >>> add = defs["add"]
+        >>> add(1, 41)
+        42
+        >>> multiply = defs["multiply"]
+        >>> multiply(2, 21)
+        42
+        """
+        source = "\n".join([c.source for c in self._nb.cells if c.cell_type == "code"])
         module = parso.parse(source)
 
         defs = [
-            def_.get_code() for def_ in itertools.chain(
-                module.iter_classdefs(), module.iter_funcdefs())
+            def_.get_code()
+            for def_ in itertools.chain(module.iter_classdefs(), module.iter_funcdefs())
         ]
 
-        defs_source = '\n'.join(defs)
+        defs_source = "\n".join(defs)
 
         with self:
             existing = set(self._shell.user_ns)
             self._shell.run_cell(defs_source)
-            defs = {
-                k: v
-                for k, v in self._shell.user_ns.items() if k not in existing
-            }
+            defs = {k: v for k, v in self._shell.user_ns.items() if k not in existing}
 
         return defs
 
@@ -239,20 +313,21 @@ class PloomberClient:
 
         # make sure that the current working directory is in the sys.path
         # in case the user has local modules
-        with add_to_sys_path('.'):
+        with add_to_sys_path("."):
             for index, cell in enumerate(self._nb.cells):
-                if cell.cell_type == 'code':
-                    self.execute_cell(cell,
-                                      cell_index=index,
-                                      execution_count=execution_count,
-                                      store_history=False)
+                if cell.cell_type == "code":
+                    self.execute_cell(
+                        cell,
+                        cell_index=index,
+                        execution_count=execution_count,
+                        store_history=False,
+                    )
                     execution_count += 1
 
         return self._nb
 
     def __enter__(self):
-        """Initialize shell
-        """
+        """Initialize shell"""
         if self._shell is None:
             self._shell = PloomberShell()
             return self
@@ -260,14 +335,12 @@ class PloomberClient:
             raise RuntimeError("A shell is already active")
 
     def __exit__(self, exc_type, exc_value, traceback):
-        """Clear shell
-        """
+        """Clear shell"""
         self._shell.clear_instance()
         self._shell = None
 
 
 class PloomberManagedClient(PloomberClient):
-
     def __init__(self, nb_man):
         super().__init__(nb_man.nb)
         self._nb_man = nb_man
@@ -277,31 +350,33 @@ class PloomberManagedClient(PloomberClient):
 
         # make sure that the current working directory is in the sys.path
         # in case the user has local modules
-        with add_to_sys_path('.'):
+        with add_to_sys_path("."):
             for index, cell in enumerate(self._nb.cells):
-                if cell.cell_type == 'code':
+                if cell.cell_type == "code":
                     try:
                         self._nb_man.cell_start(cell, index)
-                        self.execute_cell(cell,
-                                          cell_index=index,
-                                          execution_count=execution_count,
-                                          store_history=False)
+                        self.execute_cell(
+                            cell,
+                            cell_index=index,
+                            execution_count=execution_count,
+                            store_history=False,
+                        )
                     except Exception as ex:
-                        self._nb_man.cell_exception(self._nb.cells[index],
-                                                    cell_index=index,
-                                                    exception=ex)
+                        self._nb_man.cell_exception(
+                            self._nb.cells[index], cell_index=index, exception=ex
+                        )
                         break
                     finally:
-                        self._nb_man.cell_complete(self._nb.cells[index],
-                                                   cell_index=index)
+                        self._nb_man.cell_complete(
+                            self._nb.cells[index], cell_index=index
+                        )
                         execution_count += 1
 
         return self._nb
 
 
 class IO(StringIO):
-
-    def __init__(self, initial_value='', newline='\n'):
+    def __init__(self, initial_value="", newline="\n"):
         super().__init__(initial_value=initial_value, newline=newline)
         self._values = []
 
@@ -315,8 +390,7 @@ class IO(StringIO):
 
 @contextlib.contextmanager
 def patch_sys_std_out_err():
-    """Path sys.{stout, sterr} to capture output
-    """
+    """Path sys.{stout, sterr} to capture output"""
     # keep a reference to the system ones
     stdout, stderr = sys.stdout, sys.stderr
 
