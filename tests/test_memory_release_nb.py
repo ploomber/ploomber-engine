96cc1ab8cd0c1628428865638291d42badc45818
Amine AMMOR <aamineammor@gmail.com>
2023-04-26 10:19:08 +0000

    added new metric for evaluation of memory usage and added explicit  naming and deletion of the injected variables

--- tests/test_memory_release_nb.py
+++ tests/test_memory_release_nb.py
@@ -39,42 +39,42 @@ def path_notebook(tmpdir):
     return path_notebook
 
 
-@pytest.mark.parametrize(
-    "epsilon,",
-    [
-        0.01,
-    ],
-)
-def test_if_memory_leak_within_notebook(path_notebook, epsilon):
+
+def test_if_memory_leak_within_notebook(path_notebook):
     """
     epsilon is an amount of memory that is negligeable with respect
     to the data declared on the test,
     """
-    memory_usage_start = get_current_memory_usage()
+    mem_usage_start = get_current_memory_usage()
 
-    for _ in range(2):
-        # memory_usages.append(get_current_memory_usage())
-        memory_usage_begining_loop = get_current_memory_usage()
-        client = PloomberClient.from_path(path_notebook)
+    # memory_usages.append(get_current_memory_usage())
+    client = PloomberClient.from_path(path_notebook)
 
-        # injecting new variables in namespace
-        namespace = client.get_namespace(
-            namespace=dict(a=[1 for _ in range(10**7)], b=[1 for _ in range(10**7)])
-        )
-        memory_usage_with_namespace_in_mem = get_current_memory_usage()
 
-        # executing notebook with execute method
+    new_variables = dict(a=[1 for _ in range(10**7)], b=[1 for _ in range(10**7)])
+    # injecting new variables in namespace
+    namespace = client.get_namespace(
+        namespace=new_variables
+    )
+    # executing notebook with execute method
 
-        nb_node = client.execute()
+    nb_node = client.execute()
 
-        del nb_node
-        memory_usage_after_exec_method = get_current_memory_usage()
+    mem_usage_before_deletion = get_current_memory_usage()
 
-        del namespace
-        del client
+    del nb_node
+    del namespace
+    del client
+    del new_variables
 
-    memory_usage_after_namespace_deletion = get_current_memory_usage()
+    mem_usage_end = get_current_memory_usage()
 
-    assert (
-        memory_usage_after_namespace_deletion["free"] - memory_usage_start["free"]
-    ) < epsilon
+
+    # mem_rate_consumption represents the rate of memory consumtion after the beginning of the test
+    # until the deletion of  the created variables within the test (which should be negligeable in case 
+    # of absence of memory leak) , and the memory usage from the beginning of the test until the 
+    # creation of all the (memory heavy) variables.
+    mem_rate_consumption = (mem_usage_end-mem_usage_start)/(mem_usage_before_deletion-mem_usage_start)
+    print(mem_rate_consumption)
+
+    
