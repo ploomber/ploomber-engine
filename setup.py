ac03ee5de58abc0c3393fee7e72fb9bfad659c62
Eduardo Blancas <github@blancas.io>
2023-04-28 14:23:41 -0600

    fixes ipython version

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ REQUIRES = [
     "debuglater>=1.4.4",
     # used for our debug now feature (PloomberNotebookClient)
     "nbclient",
-    # used in PloomberClient
-    "ipython",
+    # used in PloomberClient (IPython dropped compatibility for Python 3.8)
+    "ipython<=8.12.0; python_version <= '3.8'",
     # used in the experiment tracker
     "parso",
     # used in several places for manipulating notebook objects
