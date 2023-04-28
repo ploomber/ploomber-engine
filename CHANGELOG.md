a9fb256a446d1112a89df51c596f19640aa03f79
Eduardo Blancas <github@blancas.io>
2023-04-28 15:23:09 -0600

    Bumps up ploomber_engine to version 0.0.28dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # CHANGELOG
 
+## 0.0.28dev
+
 ## 0.0.27 (2023-04-28)
 
 *No changes, testing release workflow*
--- src/ploomber_engine/__init__.py
+++ src/ploomber_engine/__init__.py
@@ -1,6 +1,6 @@
 from ploomber_engine.execute import execute_notebook
 
-__version__ = "0.0.27"
+__version__ = "0.0.28dev"
 
 
 # NOTE: fully initialize papermill here to prevent circular import
