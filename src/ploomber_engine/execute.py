14b0f7e26aae85864c64fde37dbca71d309feeb9
Tony Kuo <yen.c@ploomber.io>
2023-05-30 17:54:04 -0400

    lint

--- src/ploomber_engine/execute.py
+++ src/ploomber_engine/execute.py
@@ -27,7 +27,7 @@ def execute_notebook(
     profile_memory=False,
     progress_bar=True,
     debug_later=False,
-    verbose=False, 
+    verbose=False,
     remove_tagged_cells=None,
     cwd=".",
     save_profiling_data=False,
