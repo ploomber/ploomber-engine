df88c7fce88224e2347e40386a4934447084b536
tl3119 <tl3119@nyu.edu>
2023-06-08 14:19:35 -0400

    Adding license metadata

--- setup.py
+++ setup.py
@@ -88,7 +88,9 @@ setup(
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
-    classifiers=[],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
     keywords=[],
     install_requires=REQUIRES,
     extras_require={
