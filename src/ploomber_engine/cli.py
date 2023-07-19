b3b8828960dfea47c17f9b665fa15c31844166aa
avi <avi.vajpeyi@gmail.com>
2023-07-19 14:43:59 +1200

    fix cli lint error

--- src/ploomber_engine/cli.py
+++ src/ploomber_engine/cli.py
@@ -21,14 +21,14 @@ from ploomber_engine import execute_notebook
     default=False,
     type=click.UNPROCESSED,
     help="Profile cell's runtime "
-         "(a path for the runtime profile plot can additionally be passed)",
+    "(a path for the runtime profile plot can additionally be passed)",
 )
 @click.option(
     "--profile-memory",
     default=False,
     type=click.UNPROCESSED,
     help="Profile cell's memory usage  "
-         "(a path for the runtime memory plot can additionally be passed)",
+    "(a path for the runtime memory plot can additionally be passed)",
 )
 @click.option(
     "--progress-bar/--no-progress-bar", default=True, help="Display a progress bar"
